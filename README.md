98452c09e3c49005cc12f119526dca4b73e218e3
Amandine Cimmino <amandine.cimmino@yahoo.fr>
2023-08-23 18:33:30 +0200

    projetpresque fini

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Ajax-users-simplified
 It is a simplifiedd model of Ajax requete  that worked with studi lesson.
+Un premier jet de requête AJAX
+
+Cette application a pour vocation d'effectuer une requête Ajax à partir de l'API free REQRES. 
+Pour sa réalisation j'ai utilisé l'IDE Visual Studio Code.
+Les Langages utilisés sont le HTML , le CSS et le JS.
+
+Cette Application propose des listes d'utilisateurs se trouvant dans differentes pages de l'API. 
+
+Le code JS prend en consideration une pagination qui se met à jour en fonction de la création ou la suppression des pages ustilisateurs dans l'API. 
+
+Le CSS prend en consideration une petite responsivité qui peut être améliorée. 
+
+Le loader a été récupéré sur https://loading.io/css/
+
+L'objectif est egalement de modifier la liste utilisateur.
+
+Le concept d'asynchrone ou d'API fetch est survolé dans cette application
+ 
--- ajax.js
+++ ajax.js
@@ -1,5 +1,6 @@
 const loader = '<div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>'
 
+
 //Récupération du numero de page
 
 function ShowUsers(){
@@ -15,7 +16,7 @@ function getUsers(numeroPage){
 
     
     const xhr = new XMLHttpRequest(); 
-    const url = 'https://reqres.in/api/users?delay=3&page='+numeroPage;
+    const url = 'https://reqres.in/api/users?delay=2&page='+numeroPage;
     xhr.open('GET', url);
 
     xhr.addEventListener('readystatechange', function() {
@@ -41,7 +42,7 @@ setUsersInPage(object);
 function setUsersInPage(listUsers){
     let myHtml=""
     listUsers.data.forEach(element =>{
-       myHtml +='<div class="card"><div class="card-header"><h5 class="card-title">'+element.first_name+'</h5><h5 class="card-title"> ' +element.last_name+'</h5></div><div class="card-body">'+'<img src="'+element.avatar+'"></div><div class="card-footer">'+element.email+'</div></div>'
+       myHtml +='<div class="card"><div class="card-header"><h5 class="card-title">'+element.first_name+'</h5><h5 class="card-title"> ' +element.last_name+'</h5></div><div class="card-body">'+'<img src="'+element.avatar+'"></div><div class="card-footer"><div>'+element.email+'</div><div><button class ="change orange" onclick="editUser()"> </button><button class ="change rouge"onclick="deleteUser()"></button></div></div></div>'
     });
     document.getElementById("catalogue-card").innerHTML=myHtml
 
@@ -65,7 +66,116 @@ function setUsersInPage(listUsers){
     document.getElementById("numberPageUsers").setAttribute('max',nbPage);
 
 }
-
+// au chargement affiché la page 1
 document.addEventListener("DOMContentLoaded", function() {
     getUsers(1);
   });
+
+function createUSer(){
+    // requete POST 
+    const xhr = new XMLHttpRequest(); 
+    const url = 'https://reqres.in/api/users';
+    xhr.open('POST', url);
+    xhr.setRequestHeader("Content-type","application/json;charset=UTF-8");
+    xhr.addEventListener('readystatechange', function() {
+        if(xhr.readyState === 4) {
+            if (xhr.status===201){
+                // gestion du retour de l'appel Ajax
+                console.log('response = ' + xhr.response);
+                const object= JSON.parse(xhr.response);
+                console.log(object)
+                                
+            }       
+            else if(xhr.status === 404){
+                alert(`impossible de trouver l'URL`)
+            }
+            else{
+            alert(`une erreur est survenue`)
+            }
+        };
+    });
+// contenu de la requête
+    /*let myForm ={
+        name:"Amandine",
+        job:"Dev-Student"
+    }*/
+    let myForm = new FormData();
+    myForm.append('name', 'Amandine');
+    myForm.append('job', 'Dev-Student');
+    let object = {};
+    myForm.forEach((value,key) => object[key]=value);
+    let json =JSON.stringify(object);
+    xhr.send(json);
+}
+
+function createUserApiFetch(){
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json")
+   
+    const body = JSON.stringify({
+        name: document.getElementById("Nom").value,
+        job: document.getElementById("Job").value
+    }); 
+    
+    const init = { 
+        method: 'POST',
+        headers: headers,
+        body: body };
+    
+    fetch('https://reqres.in/api/users', init)
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            alert(response)
+            console.log(response)
+        })
+        .catch(error => alert("Erreur : " + error));
+}
+
+function deleteUser(){
+    const headers = new Headers();
+   
+    const init = { 
+        method: 'DELETE',
+        headers: headers
+    };
+    
+    fetch('https://reqres.in/api/users/7', init)
+        .then(response => {
+            if(response.status == 204){
+                alert("L'utilisateur a bien été supprimé");
+            }
+            else{
+                alert("Impossible de supprimer l'utilisateur");
+            }
+        })
+        .catch(error => alert("Erreur : " + error));
+}
+
+function editUser(){
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json")
+    headers.append("Authentification", "Bearer POUYDPOSY ODISUYD FGD KJFDF P")
+   
+    const body = JSON.stringify({
+        name: document.getElementById("Nom").value,
+        job: document.getElementById("Job").value
+    }); 
+    
+    const init = { 
+        method: 'PUT',
+        headers: headers,
+        body: body };
+    
+    fetch('https://reqres.in/api/users/2', init)
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            alert(response)
+            console.log(response)
+        })
+        .catch(error => alert("Erreur : " + error));
+}
+
